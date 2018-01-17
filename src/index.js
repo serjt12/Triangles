c991077f3c23da8204f214e4c891322f8a0b1acf
Andres Saldarriaga <jandreus87@hotmail.com>
2018-01-17 15:18:43 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './index.css';
+
+
+const App = () => (
+  <div className="triangle">
+    
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
